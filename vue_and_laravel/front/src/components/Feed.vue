34dc77dc4021a550be2f9fb3ef9c5f135bad7cc7
php-user1 <test@test.com>
2018-04-29 19:25:11 -0400

    Product Feed

--- vue_and_laravel/front/src/components/Feed.vue
+++ vue_and_laravel/front/src/components/Feed.vue
@@ -2,25 +2,16 @@
     <div>
         <h1 class="mb-4">Feed</h1>
 
-        <ul class="list-unstyled list-group">
-            <li v-for="product in products" class="list-group-item mb-2">{{ product.name }} | {{ product.price }}</li>
-        </ul>
+        <my-products></my-products>
     </div>
 </template>
 
 <script>
-    export default {
-        data() {
-            return {
-                products: []
-            }
-        },
+    import Products from './product/Products.vue';
 
-        created() {
-            this.$http.get('api/products')
-                .then(response => {
-                    this.products = response.body;
-                });
+    export default {
+        components: {
+            'my-products': Products
         }
     }
 </script>
---
+++ vue_and_laravel/front/src/components/product/Product.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="col-md-4 mb-3">
+        <div class="card">
+            <img src="http://images.amazon.com/images/G/01/electronics/detail-page/hp-a67xx-hero_monitor.jpg" class="img-fluid">
+
+            <div class="card-body">
+                <h4>{{ product.name }}</h4>
+                <p>${{ product.price }}</p>
+                <hr>
+                <p class="text-center">
+                    <a href="#" class="btn btn-outline-secondary">Wish list</a>
+                    <a href="#" class="btn btn-success">Buy</a>
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['product']
+    }
+</script>
