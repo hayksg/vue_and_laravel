bfa1fbb99773cf52c5ba2b43fd7e90326cde4460
php-user1 <test@test.com>
2018-04-30 18:27:20 -0400

    Navbar

--- vue_and_laravel/front/src/components/Navbar.vue
+++ vue_and_laravel/front/src/components/Navbar.vue
@@ -2,28 +2,29 @@
     <div id="app">
         <div class="navbar navbar-expand-lg fixed-top navbar-light bg-light">
             <div class="container">
-                <a href="/#/" class="navbar-brand">Bootswatch</a>
+                <router-link to="/" class="navbar-brand">Bootswatch</router-link>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     
                     <ul class="navbar-nav">
-                
-                        <li class="nav-item">
-                            <a class="nav-link" href="/#/feed">Products</a>
-                        </li>
-
+                        <router-link to="/feed" tag="li" class="nav-item" v-if="isAuth">
+                            <a class="nav-link">Products</a>
+                        </router-link>
                     </ul>
 
                     <ul class="nav navbar-nav ml-auto">
-                        <li class="nav-item">
-                            <a class="nav-link" href="/#/login">Login</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/#/register">Register</a>
-                        </li>
-                
+                        <router-link to="/login" tag="li" class="nav-item" v-if="!isAuth">
+                            <a class="nav-link">Login</a>
+                        </router-link>
+                        <router-link to="/register" tag="li" class="nav-item" v-if="!isAuth">
+                            <a class="nav-link">Register</a>
+                        </router-link>
+
+                        <router-link to="/logout" tag="li" class="nav-item" v-if="isAuth">
+                            <a class="nav-link">Logout</a>
+                        </router-link>
                     </ul>
 
                 </div>
@@ -33,16 +34,15 @@
 </template>
 
 <script>
-export default {
-    name: 'app',
-    data () {
-        return {
-      
+    export default {
+        data() {
+            return {
+                isAuth: null
+            }
+        },
+
+        created() {
+            this.isAuth = this.$auth.isAuthenticated()
         }
     }
-}
 </script>
-
-<style>
-
-</style>
